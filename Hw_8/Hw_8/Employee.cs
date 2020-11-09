8727422e6a2624b13887502b15f1bd6bb35d0635
nenedarmo@gmail.com <nenedarmo@gmail.com>
2020-11-09 17:28:02 +0200

    wh8

---
+++ Hw_8/Hw_8/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw_8
+{
+    class Employee : Person
+    {
+        public DateTime DateStartWork { get; set; }
+        public int SalaryParHour { get; set; }
+        public int SumOfHourInWeak { get; set; }
+        public int Salary { get; set; }
+
+        public Employee():base()
+        {
+
+        }
+
+        public Employee(DateTime dateStartWork, int salaryParHour, int sumOfHourInWeak, int salary, string name, string phoneNumber, long id, string adress, DateTime birthDate) : base(name, phoneNumber, id, adress, birthDate)
+       
+        {
+            DateStartWork = dateStartWork;
+            SalaryParHour = salaryParHour;
+            SumOfHourInWeak = sumOfHourInWeak;
+            Salary = salary;
+        }
+
+        public override void PrintDetails()
+        {
+            Console.WriteLine("name:" + this.Name);
+            Console.WriteLine("phoneNumber:" + this.PhoneNumber);
+            Console.WriteLine("id:" + this.Id);
+            Console.WriteLine("adress:" + this.Adress);
+            Console.WriteLine("birthDate:" + this.BirthDate);
+        }
+    }
+}
--- Hw_8/Hw_8/Student.cs
+++ Hw_8/Hw_8/Student.cs
@@ -6,18 +6,20 @@ using System.Threading.Tasks;
 
 namespace Hw_8
 {
-   abstract class Student:Person
+    class Student:Person
     {
         public string Subject { get; set; }
+        public double Avg { get; set; }
 
         public Student():base()
         {
 
         }
 
-        protected Student(string subject, string name, string phoneNumber, long id, string adress, DateTime birthDate) :base(name,phoneNumber,id,adress,birthDate)
+        protected Student(string subject, double avg, string name, string phoneNumber, long id, string adress, DateTime birthDate) :base(name,phoneNumber,id,adress,birthDate)
         {
             Subject = subject;
+            Avg = avg;
         }
 
         public override void PrintDetails()
@@ -28,11 +30,19 @@ namespace Hw_8
             Console.WriteLine("adress:" + this.Adress);
             Console.WriteLine("birthDate:" + this.BirthDate);
             Console.WriteLine("subject:" + this.Subject);
+            Console.WriteLine("avg:" + this.Avg);
         }
+
         public new  void GetDetails()
         {
             Console.WriteLine(" enter Subject:");
             Subject = Console.ReadLine();
+            Console.WriteLine(" enter avg:");
+            Avg = double.Parse(Console.ReadLine());
+
         }
-        }
+
+       
+    }
+    
 }

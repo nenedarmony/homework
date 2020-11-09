d67e1e33c7fc46a56cd8c6c169b3108ed3ce0e9f
nenedarmo@gmail.com <nenedarmo@gmail.com>
2020-11-09 16:59:27 +0200

    home work 8

---
+++ Hw_8/Hw_8/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw_8
+{
+   abstract class Person
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public long Id { get; set; }
+        public string Adress { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        protected Person(string name, string phoneNumber, long id, string adress, DateTime birthDate)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Id = id;
+            Adress = adress;
+            BirthDate = birthDate;
+        }
+
+      public  abstract void PrintDetails();
+        public  void GetDetails()
+        {
+            Console.WriteLine(" enter name:" );
+            Name = Console.ReadLine();
+            Console.WriteLine(" enter phoneNumber:" );
+            PhoneNumber = Console.ReadLine();
+            Console.WriteLine("enter id:");
+            Id = long.Parse(Console.ReadLine());
+            Console.WriteLine("enter adress:");
+            Adress = Console.ReadLine();
+            Console.WriteLine("enter birthDate:" );
+            BirthDate = DateTime.Parse(Console.ReadLine());
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Hw_8/Hw_8/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw_8
+{
+   abstract class Student:Person
+    {
+        public string Subject { get; set; }
+
+        public Student():base()
+        {
+
+        }
+
+        protected Student(string subject, string name, string phoneNumber, long id, string adress, DateTime birthDate) :base(name,phoneNumber,id,adress,birthDate)
+        {
+            Subject = subject;
+        }
+
+        public override void PrintDetails()
+        {
+            Console.WriteLine("name:"+this.Name);
+            Console.WriteLine("phoneNumber:" + this.PhoneNumber);
+            Console.WriteLine("id:" + this.Id);
+            Console.WriteLine("adress:" + this.Adress);
+            Console.WriteLine("birthDate:" + this.BirthDate);
+            Console.WriteLine("subject:" + this.Subject);
+        }
+        public new  void GetDetails()
+        {
+            Console.WriteLine(" enter Subject:");
+            Subject = Console.ReadLine();
+        }
+        }
+}
